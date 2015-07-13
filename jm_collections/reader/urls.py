d69b615d72ec98ffac4e7a1ee49c5d0889a94844
John Merigliano <jmerigliano@gmail.com>
2015-07-12 21:45:20 -0400

    cleanup

--- jm_collections/reader/urls.py
+++ jm_collections/reader/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns(
     # /reader/overview/
     url(r'^overview/$', views.OverviewView.as_view(), name='overview'),
     # /reader/add/
-    url(r'^add/$', views.ReaderAdd.as_view(), name='add'),
+    url(r'^add/$', views.ReaderAdd.as_view(), name='add')
 )
