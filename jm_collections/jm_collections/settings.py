92e60e01a0e4663981913683e5985c6306cac8ea
John Merigliano <jmerigliano@gmail.com>
2015-07-13 12:17:24 -0400

    changed db to postgres; updated link style

--- jm_collections/jm_collections/settings.py
+++ jm_collections/jm_collections/settings.py
@@ -1,20 +1,10 @@
 """
 Django settings for jm_collections project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # This is a development key only
 SECRET_KEY = 'dev'
@@ -28,7 +18,6 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -57,8 +46,6 @@ WSGI_APPLICATION = 'jm_collections.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -71,8 +58,6 @@ DATABASES = {
 }
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'US/Eastern'
@@ -83,9 +68,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
     '~/Documents/Programming/python/django-projs/jm_collections/static/',
