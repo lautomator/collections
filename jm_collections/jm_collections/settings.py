bc46171c6a4dd1fca674d49a4ebb56c4816fef6c
John Merigliano <jmerigliano@gmail.com>
2015-07-21 08:27:07 -0400

    initial memchached setup

--- jm_collections/jm_collections/settings.py
+++ jm_collections/jm_collections/settings.py
@@ -58,6 +58,14 @@ DATABASES = {
     }
 }
 
+# Memchached
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
 # Internationalization
 LANGUAGE_CODE = 'en-us'
 
